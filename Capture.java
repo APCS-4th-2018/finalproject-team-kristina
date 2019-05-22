f4c5c7d8255da555810428de90cad1af2dc562fb
kylie <coolkylie@gmail.com>
2019-05-22 12:34:52 -0700

    Added java doc for each method

--- Capture.java
+++ Capture.java
@@ -13,19 +13,33 @@ public class Capture
     public Capture()
     {
     }
-    
+
+    /**
+     *  Helper method for when stones are captured
+     */
     private void captured()
     {
     }
-    
+
+    /**
+     *  Move method specifically for capture game type (how the stones move)
+     */
     public void move()
     {
     }
-    
+
+    /**
+     *  Returns whether the user has won or lost in Capture mode.
+     *
+     * @return    The true or false depending on win or lose
+     */
     public boolean isWon()
     {
     }
-    
+
+    /**
+     *  Fills the background of the screen for Capture games
+     */
     public void fillBackground()
     {
     }
