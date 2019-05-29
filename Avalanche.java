ce58041e3fa855a13b21373293ada272442c0dda
Kristina Lansang <klansang83@gmail.com>
2019-05-29 12:56:06 -0700

    added numbers

--- Avalanche.java
+++ Avalanche.java
@@ -1,25 +1,6 @@
-import java.util.*;
-import javafx.application.Application;
-import javafx.application.Platform;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.*;
-import javafx.scene.text.Font;
-import javafx.scene.text.Text;
 import javafx.stage.Stage;
-import javafx.scene.paint.Color;
-import javafx.scene.image.Image;
-import javafx.scene.image.ImageView;
-import java.io.FileInputStream; 
-import java.io.FileNotFoundException;
-import javafx.scene.canvas.Canvas;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage; 
+import javafx.scene.layout.Pane; 
 /**
  * The Avalanche mode of the mancala game
  *
@@ -28,11 +9,6 @@ import javafx.stage.Stage;
  */
 public class Avalanche extends Mancala
 {
-    @Override
-    public void start(Stage stage) throws Exception
-    {
-        
-    }
     /**
      * Constructor for objects of class Avalanche
      */
