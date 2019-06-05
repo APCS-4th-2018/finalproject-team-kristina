1677b9e8e7c586f14b68e6d17036d6ba788d2f38
Helen <42685880+hhhelennn@users.noreply.github.com>
2019-06-04 17:16:38 -0700

    added private helper method

--- Capture.java
+++ Capture.java
@@ -20,8 +20,31 @@ public class Capture extends Mancala
     /**
      *  Helper method for when stones are captured
      */
-    private void captured()
+    private void captured(int num, int next)
     {
+        
+        int size, across, diff, originalSize;
+        if(player == PLAYER1 && next >= 1 && next <= 6)
+        {
+            originalSize = board[0].size();
+            board[0].add(board[next].remove(0));
+            across = 13 - next + 1;
+            size = board[across].size();
+            for(int i = 0; i < size; i++)
+                board[0].add(board[across].remove(0));
+            drawStones(0, size + 1 + originalSize);
+        }
+        else if(player == PLAYER2)
+        {
+            originalSize = board[7].size();
+            board[7].add(board[next].remove(0));
+            drawStones(7, 1);
+            across = 13 - next + 1;
+            size = board[across].size();
+            for(int i = 0; i < size; i++)
+                board[7].add(board[across].remove(0));
+            drawStones(7, size + 1 + originalSize);
+        }
     }
 
     /**
@@ -29,34 +52,35 @@ public class Capture extends Mancala
      */
     public void move(int num)
     {
+        //int next = (num + board[num].size()) % 14;
+        //int size, across, diff, originalSize;
         int next = (num + board[num].size()) % 14;
-        int size, across, diff, originalSize;
         super.move(num);
-        //hello if you're looking at this know that it doesn't capture all the stones from the opposite pit for some reason
         if(board[next].size() == 1)//if where you end up in an empty pit
         {
             //super.switchPlayers();//keep on the same player
-            if(player == PLAYER1 && next >= 1 && next <= 6)
-            {
-                originalSize = board[0].size();
-                board[0].add(board[next].remove(0));
-                across = 13 - next + 1;
-                size = board[across].size();
-                for(int i = 0; i < size; i++)
-                    board[0].add(board[across].remove(0));
-                drawStones(0, size + 1 + originalSize);
-            }
-            else if(player == PLAYER2)
-            {
-                originalSize = board[7].size();
-                board[7].add(board[next].remove(0));
-                drawStones(7, 1);
-                across = 13 - next + 1;
-                size = board[across].size();
-                for(int i = 0; i < size; i++)
-                    board[7].add(board[across].remove(0));
-                drawStones(7, size + 1 + originalSize);
-            }
+            // if(player == PLAYER1 && next >= 1 && next <= 6)
+            // {
+            // originalSize = board[0].size();
+            // board[0].add(board[next].remove(0));
+            // across = 13 - next + 1;
+            // size = board[across].size();
+            // for(int i = 0; i < size; i++)
+            // board[0].add(board[across].remove(0));
+            // drawStones(0, size + 1 + originalSize);
+            // }
+            // else if(player == PLAYER2)
+            // {
+            // originalSize = board[7].size();
+            // board[7].add(board[next].remove(0));
+            // drawStones(7, 1);
+            // across = 13 - next + 1;
+            // size = board[across].size();
+            // for(int i = 0; i < size; i++)
+            // board[7].add(board[across].remove(0));
+            // drawStones(7, size + 1 + originalSize);
+            // }
+            captured(num, next);
             drawNumbers();
             //isWon();
             //super.switchPlayers();//switchs back
